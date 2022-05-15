a70d173871c6d3df9d428c2b3bb270f19ec2ff74
Debleena18 <sarkardebleena18@gmail.com>
2022-05-15 13:37:39 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Tmdb from './Tmdb';
+import MovieRow from './components/Row/MovieRow';
 import './App.css';
+import FeaturedMovie from './components/Movies/FeaturedMovie';
+import Header from './components/Header/Header';
 
-function App() {
+const App = () => {
+
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
+  useEffect(() => {
+    const loadAll = async () => {
+      // pegando a lista total
+      let list = await Tmdb.getHomeList();
+      console.log(list);
+      setMovieList(list);
+
+      // Pegando o filme em destaque
+      let originals = list.filter(i => i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedData(chosenInfo);
+
+      console.log(chosenInfo);
+    }
+
+    loadAll();
+  }, []);
+
+  useEffect(() => {
+    const scrollListener = () => {
+      if (window.scrollY > 10) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+    }
+    window.addEventListener('scroll', scrollListener);
+    return () => {
+      window.removeEventListener('scroll', scrollListener);
+    }
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className='page'>
+      <Header black={blackHeader} />
+      {featuredData &&
+        <FeaturedMovie item={featuredData} />}
+      <section className='lists'>
+        {movieList.map((item, key) => (<MovieRow key={key} title={item.title} items={item.items} />))}
+      </section>
 
+      {movieList.length <= 0 &&
+        <div className='loading'>
+          <img src='https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif' alt='Carregando' size={13}></img>
+        </div>
+      }
+    </div>
+  )
+}
 export default App;
---
+++ src/Tmdb.js
@@ -0,0 +1,73 @@
+
+const API_KEY = 'fc5c1a7d52257e4d463943612cbb1366';
+const API_BASE = 'https://api.themoviedb.org/3';
+
+const basicFetch = async (endpoint) => {
+    const req = await fetch(`${API_BASE}${endpoint}`);
+    const json = await req.json();
+    return json;
+}
+
+export default {
+    getHomeList: async () => {
+        return [
+            {
+                slug: 'originals',
+                title: 'Originais do Netflix',
+                items: await basicFetch(`/discover/tv?with_networks=213&language=en-US&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'sci-fi',
+                title: 'Fiction',
+                items: await basicFetch(`/discover/movie?with_genres=878&language=en-US&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'fantasy',
+                title: 'Fantasy',
+                items: await basicFetch(`/discover/movie?with_genres=14&language=en-US&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'action',
+                title: 'Action',
+                items: await basicFetch(`/discover/movie?with_genres=28&language=en-US&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'comedy',
+                title: 'Comedi',
+                items: await basicFetch(`/discover/movie?with_genres=35&language=en-US&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'horror',
+                title: 'Horror',
+                items: await basicFetch(`/discover/movie?with_genres=27&language=en-US&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'romance',
+                title: 'Romance',
+                items: await basicFetch(`/discover/movie?with_genres=10749&language=en-US&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'documentary',
+                title: 'Documentary',
+                items: await basicFetch(`/discover/movie?with_genres=99&language=en-US&api_key=${API_KEY}`)
+            },
+        ];
+    },
+    getMovieInfo: async (movieId, type) => {
+        let info = {};
+        if (movieId) {
+            switch (type) {
+                case 'movie':
+                    info = await basicFetch(`/movie/${movieId}?language=en-US&api_key=${API_KEY}`)
+                    break;
+                case 'tv':
+                    info = await basicFetch(`/tv/${movieId}?language=en-US&api_key=${API_KEY}`)
+                    break;
+                default:
+                    info = null;
+                    break;
+            }
+        }
+        return info;
+    }
+}
